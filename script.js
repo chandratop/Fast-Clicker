c8835b79044c3115fa0d61fd21892e2128080c7e
chandratop <chandratop.mail@gmail.com>
2020-05-17 20:16:23 +0530

    Changed to neumorphic, made mobile first

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ window.onload = function () {
 
     var clock = document.getElementById("clock");
     var clickbox = document.getElementById("clickbox");
+    var cpsbox = document.getElementById("cps-box");
     var clickdiv = document.getElementById("clicks");
-    clickdiv.innerHTML = "0 clicks"
 
     clickbox.addEventListener("click", start);
     clickbox.addEventListener("click", updateCount)
@@ -15,8 +15,7 @@ window.onload = function () {
     
     function startClock() {
         clock.innerHTML = "15s";
-        clickbox.removeEventListener("click", start)
-        clickdiv.classList.toggle("hidden");
+        clickbox.removeEventListener("click", start);
         clockvar1 = setInterval(updateClock, 1000);
         setTimeout(startCPS, 1000);
     };
@@ -30,8 +29,8 @@ window.onload = function () {
             clickbox.removeEventListener("click", updateClock);
             clickbox.removeEventListener("click",updateClickBox);
             clickbox.removeEventListener("click",updateCount);
-            var resetButton = document.getElementById("reset");
-            resetButton.classList.toggle("hidden");
+            // var resetButton = document.getElementById("reset");
+            // resetButton.classList.toggle("hidden");
         }
     };
     
@@ -41,18 +40,18 @@ window.onload = function () {
 
     function updateClickBox() {
         if (current == 15) {
-            clickbox.innerHTML = "...";
+            cpsbox.innerHTML = "...";
         }
         else {
             var cps = count / (16 - current);
             cps = cps.toPrecision(3);
-            clickbox.innerHTML = cps.toString() + " CPS";
+            cpsbox.innerHTML = cps.toString() + "<br>" + "CPS";
         }
     }
 
     function updateCount() {
         ++count;
-        clickdiv.innerHTML = count.toString();
+        clickdiv.innerHTML = count.toString() + "<br>" + "clicks";
     }
 
     function start() {
